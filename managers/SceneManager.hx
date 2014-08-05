972c2c963a267fff7d48e6ea1d4dfadffe9d9d23
ARebel-PC <karlo@accidentalrebel.com>
2014-08-05 21:51:50 +0800

    Created a SceneManager class

---
+++ managers/SceneManager.hx
@@ -0,0 +1,24 @@
+package flambepowertools.managers;
+import flambe.Entity;
+import flambe.scene.Director;
+import flambe.scene.Scene;
+import flambe.System;
+
+class SceneManager
+{
+	static private var _sceneDirector:Director;
+	
+	static public function setup()
+	{
+		_sceneDirector = new Director();
+		System.root.add(_sceneDirector);
+	}
+	
+	static public function switchScene(toScene : Scene)
+	{
+		var newEntity : Entity = new Entity();
+		newEntity.add(toScene);
+		
+		_sceneDirector.unwindToScene(newEntity);
+	}
+}
