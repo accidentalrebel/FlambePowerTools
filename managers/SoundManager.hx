81fd3981dd86b7d00c2b8399b70dae969331334d
ARebel-PC <karlo@accidentalrebel.com>
2014-08-25 22:13:54 +0800

    Added a playAudio function

--- managers/SoundManager.hx
+++ managers/SoundManager.hx
@@ -15,27 +15,32 @@ class SoundManager
 		_playbackMap = new Map<String, Playback>();
 	}
 	
-	static public function loopAudio(bgmPath : String, restart : Bool = false)
+	static public function playAudio(audioPath:String) 
+	{
+		Registry.assetPack.getSound(audioPath).play();
+	}
+	
+	static public function loopAudio(audioPath : String, restart : Bool = false)
 	{
 		if ( restart )
-			stopAudio(bgmPath);
+			stopAudio(audioPath);
 		
-		var playback : Playback = _playbackMap.get(bgmPath);
+		var playback : Playback = _playbackMap.get(audioPath);
 		if ( playback == null ) {
-			playback = Registry.assetPack.getSound(bgmPath).loop();
-			_playbackMap.set(bgmPath, playback);
+			playback = Registry.assetPack.getSound(audioPath).loop();
+			_playbackMap.set(audioPath, playback);
 		}
 		else {
 			playback.sound.loop();
 		}
 	}
 	
-	static public function stopAudio(bgmPath : String)
+	static public function stopAudio(audioPath : String)
 	{
-		var playback : Playback = _playbackMap.get(bgmPath);
+		var playback : Playback = _playbackMap.get(audioPath);
 		if ( playback != null ) {
 			playback.dispose();
-			_playbackMap.remove(bgmPath);
+			_playbackMap.remove(audioPath);
 		}
 	}
 }
