eeae340876697271a9ed10cd3b3591731b77f428
ARebel-PC <karlo@accidentalrebel.com>
2014-09-05 22:14:31 +0800

    Created a simple button class

---
+++ ui/Button.hx
@@ -0,0 +1,63 @@
+package flambepowertools.ui;
+import flambe.display.FillSprite;
+import flambe.display.ImageSprite;
+import flambe.display.Sprite;
+import flambe.input.PointerEvent;
+import flambe.script.CallFunction;
+import flambe.script.Script;
+import flambe.System;
+import game.Registry;
+
+/**
+ * ...
+ * @author Karlo
+ */
+class Button extends ImageSprite
+{	
+	static private inline var ON_PRESS_ANIMATION_SPEED:Float = 0.10;
+	static private inline var ON_PRESS_SCALE_TARGET : Float = 0.90;
+	
+	var _onPressedAnimationScript:Script;
+	
+	public function new(?texturePath : String) 
+	{
+		super(Registry.assetPack.getTexture(texturePath));		
+		this.centerAnchor();
+	}
+	
+	override public function onAdded() 
+	{
+		super.onAdded();
+		
+		setupPressListeners();
+	}
+	
+	// ============================================= SETUP ============================================= //
+	function setupPressListeners() 
+	{
+		this.pointerDown.connect(onPressedDown);
+	}
+	
+	// ============================================= EVENTS ============================================= //
+	function onPressedDown(pointerEvent : PointerEvent) 
+	{
+		playOnPressedAnimation();
+	}
+	
+	// ============================================= ANIMATIONS ============================================= //
+	function playOnPressedAnimation() 
+	{		
+		if ( _onPressedAnimationScript != null )
+			_onPressedAnimationScript.dispose();
+		
+		this.scaleX.animateTo(ON_PRESS_SCALE_TARGET, ON_PRESS_ANIMATION_SPEED);
+		this.scaleY.animateTo(ON_PRESS_SCALE_TARGET, ON_PRESS_ANIMATION_SPEED);		
+		
+		_onPressedAnimationScript = MiscUtils.callWithDelay(owner, new CallFunction(playGrowBackAnimation), ON_PRESS_ANIMATION_SPEED);
+	}
+	
+	function playGrowBackAnimation() {
+		this.scaleX.animateTo(1, ON_PRESS_ANIMATION_SPEED);
+		this.scaleY.animateTo(1, ON_PRESS_ANIMATION_SPEED);
+	}
+}
